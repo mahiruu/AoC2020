0a2b117cd97869dc796e3fe16bbade2766a7f8db
machan <zahir.ilyava@protonmail.com>
2022-09-10 01:39:09 +0200

    ...

--- AoC2020/Program.cs
+++ AoC2020/Program.cs
@@ -1,8 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿// AoC 2020 solutions
+// made by ma-chan
 
 //Console.WriteLine(AoC2020.day1.Part1.GetResult());
 //Console.WriteLine(AoC2020.day1.Part2.GetResult());
 //Console.WriteLine(AoC2020.day2.Part1.GetResult());
+//Console.WriteLine(AoC2020.day2.Part2.GetResult());
 
-Console.WriteLine(AoC2020.day2.Part2.GetResult());
